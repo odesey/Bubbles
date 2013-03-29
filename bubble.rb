5d1f0aa9d3762b9f8a6f42dec8562672925ce628
odesey <odesey@gmail.com>
2013-03-28 21:59:33 -0400

    Slowed down the nested loop

--- bubble.rb
+++ bubble.rb
@@ -9,8 +9,9 @@ while cycle > 0 do #we only need to go through the outter loop once, we will sto
 			if to_sort[x] > to_sort[x+1] #compare if the 2 selected values are in order
 		
 				to_sort[x], to_sort[x+1] = to_sort[x+1], to_sort[x] #if the 1st value is greater than the 2nd value then swap values
-
-				puts "**********RESORT**************" #puts some feedback on the screen
+				a = (1..30000000).to_a #slow down the loop a bit for dramatic effect!
+				a
+				puts "**********RESORTING**************" #puts some feedback on the screen
 				puts to_sort.join ',' #puts all the values of the array on one line instead of 1 per line 
 				else 
 			end 
